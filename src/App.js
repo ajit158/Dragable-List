03853eace56bb4e10435b832380bdb2c86a708e4
Ajit Kumar <ajitk4305@gmai.com>
2021-07-03 01:56:39 +0530

    Draggable list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import data from "./data/data";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+
+require("react-dom");
+window.React2 = require("react");
+console.log(window.React1 === window.React2);
 
 function App() {
+  const [list, setList] = useState(data);
+  console.log(list);
+
+  const onDrgEnd = (result) => {
+    if (!result.destination) return;
+
+    const items = Array.from(list);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    setList(items);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <DragDropContext onDragEnd={onDrgEnd}>
+        <h1>List Items</h1>
+        <Droppable droppableId="1234">
+          {(provided, snapshot) => (
+            <div ref={provided.innerRef}>
+              {list.map((item, index) => {
+                return (
+                  <Draggable
+                    draggableId={item.id.toString()}
+                    index={index}
+                    key={index}
+                  >
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <div className="list-item">{item.name}</div>
+                      </div>
+                    )}
+                  </Draggable>
+                );
+              })}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
